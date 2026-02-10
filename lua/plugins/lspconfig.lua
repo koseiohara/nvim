726634c59587f63426cdaebca9b322ebc6693d79
koseiohara <ohara.kosei.s3@dc.tohoku.ac.jp>
2026-02-11 03:05:00 +0900

    lsp for fortran

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -5,9 +5,12 @@ return {
  
     config = function()
 
-        local tex_show_warnings = false
-        vim.keymap.set("i", "<C-n>", "<Nop>", { silent = true })
-        vim.keymap.set("i", "<C-p>", "<Nop>", { silent = true })
+        local tex_show_warnings     = false
+        local fortran_show_warnings = false
+        local python_show_warnings  = false
+
+        vim.keymap.set('i', '<C-n>', '<Nop>', { silent = true })
+        vim.keymap.set('i', '<C-p>', '<Nop>', { silent = true })
 
         vim.diagnostic.config({
             virtual_text = false,
@@ -17,29 +20,43 @@ return {
             severity_sort    = true ,
         })
 
+        -- =========================================
+        --  Diagnostic Threshold
+        -- =========================================
         local function set_tex_diagnostic_handlers(bufnr, show_warnings)
             local min = show_warnings and vim.diagnostic.severity.WARN or vim.diagnostic.severity.ERROR
             vim.diagnostic.config({
                 underline = { severity = { min = min } },
-                signs = { severity = { min = min } },
+                signs     = { severity = { min = min } },
+            }, bufnr)
+        end
+
+        local function set_fortran_diagnostic_handlers(bufnr, show_warnings)
+            local min = show_warnings and vim.diagnostic.severity.WARN or vim.diagnostic.severity.ERROR
+            vim.diagnostic.config({
+                underline = { severity = { min = min } },
+                signs     = { severity = { min = min } },
             }, bufnr)
         end
 
         -- =========================================
-        -- 2) Global toggles (language-agnostic)
+        --  Global toggles (language-agnostic)
         -- =========================================
-        vim.api.nvim_create_user_command("DiagVirtualTextToggle", function()
+        vim.api.nvim_create_user_command('DiagVirtualTextToggle', function()
             local cur = vim.diagnostic.config().virtual_text
             vim.diagnostic.config({
                 virtual_text = not cur
             })
         end, {})
 
-        -- vim.lsp.handlers["textDocument/completion"] = function()
+        -- vim.lsp.handlers['textDocument/completion'] = function()
         -- end
-        vim.lsp.handlers["textDocument/codeAction"] = function()
+        vim.lsp.handlers['textDocument/codeAction'] = function()
         end
 
+        -- =========================================
+        --  Kill LSP Annoying UI
+        -- =========================================
         local function disable_ui_capabilities(client, bufnr)
             client.server_capabilities.completionProvider = nil
             client.server_capabilities.codeActionProvider = false
@@ -53,13 +70,37 @@ return {
             -- set_tex_diagnostic_handlers(bufnr, tex_show_warnings)
         end
 
+        -- =========================================
+        --  Root Directory
+        -- =========================================
         local function tex_root(fname)
-            return vim.fs.root(fname, { ".latexmkrc", ".git" })
+            return vim.fs.root(fname, { '.latexmkrc', '.git' })
         end
 
+        local function fortran_root(fname)
+            return vim.fs.root(fname, { '.git', 'Makefile' })
+        end
+
+        -- =========================================
+        --  LSP Server Setting for Fortran
+        -- =========================================
+        vim.lsp.config('fortls', {
+            on_attach = on_attach,
+            filetypes = { 'fortran' },
+            root_dir  = fortran_root,
+
+            settings = {
+                fortls = {
+                },
+            },
+        })
+
+        -- =========================================
+        --  LSP Server Setting for LaTeX
+        -- =========================================
         vim.lsp.config('texlab', {
             on_attach = on_attach,
-            filetypes = { "tex", "plaintex", "bib" },
+            filetypes = { 'tex', 'plaintex', 'bib' },
             root_dir = tex_root,
             settings = {
                 texlab = {
@@ -70,8 +111,8 @@ return {
 
                     diagnostics = {
                         ignoredPatterns = {
-                            "Underfull \\\\hbox",
-                            "Overfull \\\\hbox",
+                            'Underfull \\\\hbox',
+                            'Overfull \\\\hbox',
                         },
                     },
 
@@ -84,29 +125,37 @@ return {
         })
 
         -- ==========================================
-        -- 6) ltex-ls（ltex）
+        --  LSP Server Setting for English
         -- ==========================================
         vim.lsp.config('ltex', {
             on_attach = on_attach,
-            filetypes = { "tex", "plaintex", "bib" },
+            filetypes = { 'tex', 'plaintex', 'bib' },
             root_dir = tex_root,
             settings = {
                 ltex = {
-                    language = "en-US",
-                    -- dictionary = { ["en-US"] = { "quasigeostrophic", ... } },
-                    -- disabledRules = { ["en-US"] = { "MORFOLOGIK_RULE_EN_US" } },
+                    language = 'en-US',
+                    -- dictionary = { ['en-US'] = { 'quasigeostrophic', ... } },
+                    -- disabledRules = { ['en-US'] = { 'MORFOLOGIK_RULE_EN_US' } },
                 },
             },
         })
 
 
         vim.lsp.enable({
+            'fortls',
             'texlab',
             'ltex',
         })
 
-        vim.api.nvim_create_autocmd("FileType", {
-            pattern = { "tex", "plaintex", "bib" },
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = { 'fortran' },
+            callback = function(args)
+                set_fortran_diagnostic_handlers(args.buf, tex_show_warnings)
+            end,
+        })
+
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = { 'tex', 'plaintex', 'bib' },
             callback = function(args)
                 set_tex_diagnostic_handlers(args.buf, tex_show_warnings)
             end,
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,14 +3,18 @@
 
 return {
     {
-        "williamboman/mason.nvim",
+        'williamboman/mason.nvim',
         opts = {},
     },
     {
-        "williamboman/mason-lspconfig.nvim",
-        dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
+        'williamboman/mason-lspconfig.nvim',
+        dependencies = { 'williamboman/mason.nvim', 'neovim/nvim-lspconfig' },
         opts = {
-            ensure_installed = { "texlab", "ltex" }, -- mason の登録名は環境で "ltex-ls" 表記の場合あり
+            ensure_installed = {
+                'texlab',
+                'ltex',
+                'fortls',
+            },
             automatic_installation = true,
         },
     },
