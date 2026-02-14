b446b3a47a9f3d0888bbf278b3b62575a39759cb
kosei <ohara.kosei.s3@dc.tohoku.ac.jp>
2026-02-14 16:57:33 +0900

    added underline to tree-sitter-context

--- lua/plugins/tscontext.lua
+++ lua/plugins/tscontext.lua
@@ -7,12 +7,18 @@ return {
         require('treesitter-context').setup({
             enable = true,
             multiwindow = false,
-            max_lines = 0,
+            max_lines = 5,
             line_numbers = true,
             multiline_threshold = 10,
-            trim_scope = 'outer',
+            trim_scope = 'inner',
             mode = 'cursor',
             -- separator = '_'
         })
+        vim.api.nvim_set_hl(0, 'TreesitterContextBottom', {
+            sp = '#7aa2f7',
+            underline = true
+        })
     end,
 }
+
+
