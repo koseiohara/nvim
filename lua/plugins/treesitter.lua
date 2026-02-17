68f8eb7483bcf6d94dbb2c1219f0b2e2562c3dbc
koseiohara <ohara.kosei.s3@dc.tohoku.ac.jp>
2026-02-17 19:51:48 +0900

    disabled treesitter on fortran

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,53 +2,50 @@
 
 return {
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
     lazy = false,
     build = function()
         if vim.fn.executable('tree-sitter') == 1 then
-            vim.cmd('TSUpdate')
+            vim.cmd(':TSUpdate')
         end
     end,
     config = function()
         require('nvim-treesitter').setup({
             install_dir = vim.fn.stdpath('data') .. '/site',
-            highlight = {
-                enable = true,
-                -- disable = {
-                --     'fortran',
-                -- },
-            },
-            indent = {
-                enable = true,
-                -- disable = {
-                --     'fortran',
-                -- },
-            },
         })
 
+        local lang = {
+            'python',
+            'fortran',
+            'lua',
+            'vim',
+            'c',
+            'bash',
+            'markdown',
+            'markdown_inline',
+            'latex',
+        }
+
         if vim.fn.executable('tree-sitter') == 1 then
-            require('nvim-treesitter').install({
-                'python',
-                'fortran',
-                'lua',
-                'vim',
-                'c',
-                'bash',
-                'markdown',
-                'markdown_inline',
-                'latex',
-            })
+            require('nvim-treesitter').install(lang)
         end
 
-        -- vim.api.nvim_create_autocmd('FileType', {
-        --     group = vim.api.nvim_create_augroup('treesitter-auto-start', { clear = true }),
-        --     pattern = {'*'},
-        --     callback = function(ev)
-        --         if vim.g.bigfile_mode then
-        --             return
-        --         end
-        --         pcall(vim.treesitter.start, ev.buf)
-        --     end,
-        -- })
+        vim.api.nvim_create_autocmd('FileType', {
+            group = vim.api.nvim_create_augroup('treesitter-auto-start', { clear = true }),
+            pattern = lang,
+            callback = function(ev)
+                if vim.g.bigfile_mode then
+                    return
+                end
+                if ev.match == 'fortran' then
+                    return
+                end
+
+                pcall(vim.treesitter.start, ev.buf)
+                vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                -- vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
+        })
     end,
 }
 
--- queries/fortran/fortran.scm
+++
@@ -1,8 +0,0 @@
-
-(do_loop) @indent
-(end_do_loop_statement) @outdent
-
-(if_statement (_) @indent)
-(end_if_statement) @outdent
-
-
