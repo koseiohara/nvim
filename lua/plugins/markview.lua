c5437fff76a326e0ce70f2d7aa156e5d0492a615
koseiohara <ohara.kosei.s3@dc.tohoku.ac.jp>
2026-03-07 01:09:19 +0900

    new plugin setting: markview

---
+++ lua/plugins/markview.lua
@@ -0,0 +1,32 @@
+
+
+return {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+    ft = {'markdown'},
+
+    config = function()
+        local markview = require('markview.commands')
+
+        require('markview').setup({
+            preview = {
+                enable = false,
+            }
+        })
+
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = 'markdown',
+            callback = function(event)
+                vim.keymap.set('n', '<leader>md', function()
+                    markview.splitToggle()
+                end, {
+                    buffer = event.buf,
+                    desc   = 'Markview: Toggle Split View',
+                    silent = true,
+                })
+            end,
+        })
+    end,
+}
+
+
